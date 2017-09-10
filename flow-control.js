d17714feab28a6136b6966215719f064e5676dc5
maude sos <mualth@gmail.com>
2017-09-10 03:27:59 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -21,13 +21,18 @@ function ageChecker(age) {
     }
     return ("You are a grownup")
 
-}
+  }
 
 function ternaryTeenager(age) {
   return (age>=13 && age<=19) ? "You are a teenager" :
   "You are not a teenager"
-}
+  }
 
 function switchAge(age) {
-
+  switch(age){
+    case 13, 14, 15, 16, 17, 18, 19:
+      return ("You are a teenager");
+    default:
+      return "You have an age"
+  }
 }
